00502af1581f2c5492c6e6aabf9744d45d9f6501
lfflorian <lfflorian4@gmail.com>
2022-01-15 19:07:53 -0600

    calling and responding torre api data

--- Backend/config/index.js
+++ Backend/config/index.js
@@ -2,6 +2,7 @@ require ('dotenv').config()
 
 const config = {
     port: process.env.PORT || 3000,
+    user_torre_api: process.env.USER_TORRE_API,
 }
 
 module.exports = { config }
--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 const { config } = require('./config/index')
 
+const userApi = require('./routes/user')
+
 app.use(express.json())
+
+userApi(app)
 
 app.listen(config.port, function () {
     console.log(`Listening on http://localhost:${config.port}`)
