924b9e8349768c579382c6dc6d206cff448b056c
lfflorian <lfflorian4@gmail.com>
2022-01-15 22:43:33 -0600

    cors implementation

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const { config } = require('./config/index')
 
 const userApi = require('./routes/user')
 
 app.use(express.json())
+app.use(cors());
 
 userApi(app)
 
