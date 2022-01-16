64cdb390f8a07d1ed27704aef3c689ffbdcaadcd
lfflorian <lfflorian4@gmail.com>
2022-01-16 06:10:41 -0600

    search endpoint implemented

--- Backend/config/index.js
+++ Backend/config/index.js
@@ -3,6 +3,7 @@ require ('dotenv').config()
 const config = {
     port: process.env.PORT || 3000,
     user_torre_api: process.env.USER_TORRE_API,
+    search_torre_api: process.env.SEARCH_TORRE_API,
 }
 
 module.exports = { config }
--- Backend/index.js
+++ Backend/index.js
@@ -4,11 +4,13 @@ const app = express()
 const { config } = require('./config/index')
 
 const userApi = require('./routes/user')
+const searchApi = require('./routes/search')
 
 app.use(express.json())
 app.use(cors());
 
 userApi(app)
+searchApi(app)
 
 app.listen(config.port, function () {
     console.log(`Listening on http://localhost:${config.port}`)
