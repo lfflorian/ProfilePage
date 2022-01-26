37bcb666227e933e6f6d0407cde0e4899eddefbd
lfflorian <lfflorian4@gmail.com>
2022-01-26 01:48:48 -0600

    loader and message components added and implemented in home

--- Frontend/ProfilePage/src/app/layout/home/home.component.ts
+++ Frontend/ProfilePage/src/app/layout/home/home.component.ts
@@ -1,5 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +11,38 @@ import { Router } from '@angular/router';
 export class HomeComponent implements OnInit {
 
   User: string;
-  constructor(private router: Router) { }
+  message: string;
+  showMessage: boolean = false;
+  showLoader: boolean = false;
+  
+  constructor(private router: Router,
+              private userService : UserService) { }
 
   ngOnInit(): void {
   }
 
   goToProfile() {
-    this.router.navigateByUrl(`profile/user/${this.User}`)
+    this.showLoader = true;
+    this.showMessage = false;
+
+    this.userService.getUser(this.User).subscribe(user => {
+      this.showLoader = false;
+      this.router.navigateByUrl(`profile/user/${this.User}`)
+    }, (error : HttpErrorResponse) => {
+      switch (error.status) {
+        case 404:
+          this.message = "User not found, please try again!";
+          break;
+        case 500:
+          this.message = "There its an error from the server, please try again later";
+          break;
+        default:
+          this.message = "There its an error, please try again later";
+          break;
+      }
+
+      this.showLoader = false;
+      this.showMessage = true;
+    })
   }
 }
--- Frontend/ProfilePage/src/app/shared/shared.module.ts
+++ Frontend/ProfilePage/src/app/shared/shared.module.ts
@@ -4,10 +4,18 @@ import { ChipComponent } from '../shared/chip/chip.component';
 import { ChipContainerComponent } from './chip-container/chip-container.component';
 import { ProfileImageComponent } from './profile-image/profile-image.component';
 import { UserContainerComponent } from './user-container/user-container.component';
+import { LoaderComponent } from './loader/loader.component';
+import { MessageBoxComponent } from './message-box/message-box.component';
 
 
 @NgModule({
-  declarations: [ChipComponent, ChipContainerComponent, ProfileImageComponent, UserContainerComponent],
+  declarations: [
+    ChipComponent,
+    ChipContainerComponent, 
+    ProfileImageComponent, 
+    UserContainerComponent, 
+    LoaderComponent,
+    MessageBoxComponent],
   imports: [
     CommonModule
   ],
@@ -15,7 +23,10 @@ import { UserContainerComponent } from './user-container/user-container.componen
     ChipComponent,
     ChipContainerComponent,
     ProfileImageComponent,
-    UserContainerComponent
+    UserContainerComponent,
+    LoaderComponent,
+    MessageBoxComponent
   ]
 })
+
 export class SharedModule { }
